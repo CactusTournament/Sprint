8d74aee535fede9a7b159168ce591f146aed4bcd
starconi <landonstorm709@gmail.com>
2025-02-23 18:14:06 -0330

    Improved Turtle

--- Turtle.py
+++ Turtle.py
@@ -1,4 +1,4 @@
-# Description: 
+# Description: This is a program that shows the uses of the turtle library, which can be used to make drawings.
 # Author: Landon
 # Date: Feb 17, 2025 - Feb 26, 2025
 
@@ -6,57 +6,98 @@
 import turtle # This library is used to create drawings/graphics with in python.
 
 # Program Constant
-t = turtle.Turtle()
-
+t = turtle.Turtle() # Constant for the turtle object
 # Display for drawing
 turtle.setup(width=500, height=500, startx=500, starty=200) # This fuction is used to create the window in which the "turtle" will draw on.
 
+# Art for page (Just for looks)
+def print_ascii_title():
+    ascii_title = r"""
+
+                        ▀█▀ █░█ █▀█ ▀█▀ █░░ █▀▀
+                        ░█░ █▄█ █▀▄ ░█░ █▄▄ ██▄
+
+"""
+    print(ascii_title)
+
+def print_ascii_art():
+    ascii_art = r"""
+                      Thank you for using Turtle,                       
+                  close the window to end the program.  
+
+                              ___-------___                         
+                          _-~~             ~~-_                     
+                       _-~                    /~-_                  
+    /^\__/^\         /~  \                   /    \\                
+  /|  O|| O|        /      \_______________/        \\              
+ | |___||__|      /       /                \          \\            
+ |          \    /      /                    \          \\          
+ |   (_______) /______/                        \_________ \\        
+ |         / /         \                      /            \\       
+  \         \^\\         \                  /               \     / 
+    \         ||           \______________/      _-_       //\__//  
+      \       ||------_-~~-_ ------------- \ --/~   ~\    || __/    
+        ~-----||====/~     |==================|       |/~~~~~       
+         (_(__/  ./     /                    \_\      \.            
+                (_(___/                         \_____)_)           
+    """
+    print(ascii_art)
+
 # User Input(s)
+print_ascii_title()
 while True:
-
-    Shape = input("What shape would you like to draw? (Square, Triangle, Circle, or Star): ").title()
-    if Shape != "Square" and Shape != "Triangle" and Shape != "Circle" and Shape != "Star":
-        print("   Data Entry Error - Please enter a valid shape.")
-    else:
-        print("   You have selected to draw a", Shape + ".")
-
     while True:
-        if Shape == "Square":
-            t.speed(1)
-            t.color("blue")
+        Shape = input("What shape would you like to draw? (Square, Triangle, Circle, or Star): ").title()
+        print()
+        if Shape in ["Square", "Triangle", "Circle", "Star"]:
+            print("   You have selected to draw a", Shape + ".")
+            print()
+            break
+        else:
+            print("   Data Entry Error - Please enter a valid shape.")
+            print()
+
+    if Shape == "Square":
+        t.speed(1) # Controls speed of turtle
+        t.color("blue") # Dictates turtles color
             
-            for i in range(4):
-                t.forward(100)
-                t.right(90)
-        else:
-            break
+        for i in range(4): # Directions for turtle to make shape
+            t.forward(100)
+            t.right(90)
 
-    while True:
-        if Shape == "Triangle":
-            t.speed(1) 
-            t.color("red")
+    elif Shape == "Triangle":
+        t.speed(1)
+        t.color("red")
                 
-            for i in range(3):
-                t.forward(100)
-                t.right(120)
-        else:
-            break
+        for i in range(3):
+            t.forward(100)
+            t.right(120)
 
-    while True:
-        if Shape == "Circle":
-            t.speed(1)     
-            t.color("green")
-            t.circle(100)
-        else:
-            break
+    elif Shape == "Circle":
+        t.speed(1)
+        t.color("green")
+        t.circle(100) # Function apart of the turtle library specificly made to make circles
 
-    while True:
-        if Shape == "Star":
-            t.speed(1)                
-            t.color("yellow")
+    elif Shape == "Star":
+        t.speed(1)
+        t.color("yellow")
                             
-            for i in range(5):
-                t.forward(100)
-                t.right(144)
-        else:
+        for i in range(5):
+            t.forward(100)
+            t.right(144)
+
+    while True:
+        exitprogram = input("Would you like to draw another shape? (Y or N): ").upper()
+        print()
+        if exitprogram in ["Y", "N"]:
             break
+        else:
+            print("   Data Entry Error - Please enter a valid response.")
+            print()
+
+    if exitprogram != "Y":
+        print()
+        print_ascii_art()
+        break
+
+turtle.done() # This function keeps the display window open until the user closes it
